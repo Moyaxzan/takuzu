a1d7457ae301cd20a553a26585a55069d1d5704d
Tao <tao.spa2003@gmail.com>
2022-04-08 14:06:57 +0200

    startingqsiuodhfdsmoqij

--- takuzu.c
+++ takuzu.c
@@ -8,10 +8,31 @@ int **create_grid(int size){
     int i;
     int j;
     int tab[size][size];
-    for(i=0;i<size;i++){
-        for(j=0;j<size;j++){
-            tab[i][j]=0;
+    for(i = 0; i < size; i++){
+        for(j=0; j < size; j++){
+            tab[i][j] = 0;
         }
     }
     return tab;
+}
+
+
+int **create_takuzu(int** tab, int size){
+    for(int i = 0; i < size; i++){
+        for(int j=0; j < size; j++){
+
+        }
+    }
+    return tab;
+}
+
+void display_grid(int **grid, int size){
+    int i;
+    int j;
+    for(i = 0; i < size; i++){
+        for(j = 0; j < size; j++){
+            printf("%d ", grid[i][j]);
+        }
+        printf("\n");
+    }
 }
